dab8a3b4e1f9225c232798679999f5692147fdc6
Daniel Arqués Toro <danielatoro@ext.inditex.com>
2024-11-03 11:31:53 +0100

    Initial commit (h2, JPA, Spring Web dependencies)

---
+++ src/main/java/com/daniarques/realnaut_spacechips/RealnautSpacechipsApplication.java
@@ -0,0 +1,13 @@
+package com.daniarques.realnaut_spacechips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealnautSpacechipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealnautSpacechipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniarques/realnaut_spacechips/RealnautSpacechipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniarques.realnaut_spacechips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealnautSpacechipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
