7ce475f3ee3cc11a0acb5244a98e3ca779bc2440
Daniel Arqués Toro <danielatoro@ext.inditex.com>
2024-11-03 13:34:39 +0100

    fix: Fix entity name

--- src/main/java/com/daniarques/realnaut_spaceships/repository/entity/ShowEntity.java
+++ src/main/java/com/daniarques/realnaut_spaceships/repository/entity/ShowEntity.java
@@ -3,7 +3,7 @@ package com.daniarques.realnaut_spaceships.repository.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
-@Entity
+@Entity(name = "show")
 public class ShowEntity {
 
 	@Id
