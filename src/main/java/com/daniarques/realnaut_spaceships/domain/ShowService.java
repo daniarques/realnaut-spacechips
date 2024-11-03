9f2f835ab330140faff6cd36b85f3a290dc6d1ef
Daniel Arqués Toro <danielatoro@ext.inditex.com>
2024-11-03 13:23:34 +0100

    refactor: Add service as an implementation of a ShowService interface

--- src/main/java/com/daniarques/realnaut_spaceships/domain/ShowService.java
+++ src/main/java/com/daniarques/realnaut_spaceships/domain/ShowService.java
@@ -1,25 +1,8 @@
 package com.daniarques.realnaut_spaceships.domain;
 
-import com.daniarques.realnaut_spaceships.domain.mapper.ShowMapper;
 import com.daniarques.realnaut_spaceships.domain.model.Show;
-import com.daniarques.realnaut_spaceships.repository.ShowRepository;
-import com.daniarques.realnaut_spaceships.repository.entity.ShowEntity;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Component;
 
-import java.util.Optional;
+public interface ShowService {
 
-@Component
-@AllArgsConstructor
-public class ShowService {
-
-	private final ShowRepository showRepository;
-	private final ShowMapper showMapper;
-
-	public Show getShowById(final Long id) {
-		final Optional<ShowEntity> entityFound = this.showRepository.findById(id);
-		return entityFound.map(this.showMapper::map)
-				// TODO 3/11/24: Throw custom exception
-				.orElse(null);
-	}
+	Show getShowById(Long id);
 }
---
+++ src/main/java/com/daniarques/realnaut_spaceships/domain/ShowServiceImpl.java
@@ -0,0 +1,26 @@
+package com.daniarques.realnaut_spaceships.domain;
+
+import com.daniarques.realnaut_spaceships.domain.mapper.ShowMapper;
+import com.daniarques.realnaut_spaceships.domain.model.Show;
+import com.daniarques.realnaut_spaceships.repository.ShowRepository;
+import com.daniarques.realnaut_spaceships.repository.entity.ShowEntity;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@AllArgsConstructor
+public class ShowServiceImpl implements ShowService {
+
+	private final ShowRepository showRepository;
+	private final ShowMapper showMapper;
+
+	@Override
+	public Show getShowById(final Long id) {
+		final Optional<ShowEntity> entityFound = this.showRepository.findById(id);
+		return entityFound.map(this.showMapper::map)
+				// TODO 3/11/24: Throw custom exception
+				.orElse(null);
+	}
+}
