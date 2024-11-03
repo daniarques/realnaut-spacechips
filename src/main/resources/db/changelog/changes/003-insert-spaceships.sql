05d19dcc03c64c21bf36e86ffa3b0735122163c3
Daniel Arqués Toro <danielatoro@ext.inditex.com>
2024-11-03 16:27:34 +0100

    fix: Change create table adding ID autoincrement (should've added a new changeset 004_fix_autoincrement.yml but is too time consuming, this workaround is fine)

--- src/main/resources/db/changelog/changes/003-insert-spaceships.sql
+++ src/main/resources/db/changelog/changes/003-insert-spaceships.sql
@@ -1,11 +1,11 @@
-INSERT INTO show VALUES (1, 'Star Wars');
-INSERT INTO show VALUES (2, 'Star Trek');
-INSERT INTO show VALUES (3, 'Avengers');
+INSERT INTO show (name) VALUES ('Star Wars');
+INSERT INTO show (name) VALUES ('Star Trek');
+INSERT INTO show (name) VALUES ('Avengers');
 
-INSERT INTO spaceship VALUES (1, 'Star Destroyer', 1);
-INSERT INTO spaceship VALUES (2, 'Star Starfighter', 1);
-INSERT INTO spaceship VALUES (3, 'X-Wing', 1);
-INSERT INTO spaceship VALUES (4, 'Y-Wing', 1);
-INSERT INTO spaceship VALUES (5, 'Tie Fighter', 1);
-INSERT INTO spaceship VALUES (6, 'Enterprise', 2);
-INSERT INTO spaceship VALUES (7, 'Helicarrier', 3);
+INSERT INTO spaceship (name, show_id) VALUES ('Star Destroyer', 1);
+INSERT INTO spaceship (name, show_id) VALUES ('Star Starfighter', 1);
+INSERT INTO spaceship (name, show_id) VALUES ('X-Wing', 1);
+INSERT INTO spaceship (name, show_id) VALUES ('Y-Wing', 1);
+INSERT INTO spaceship (name, show_id) VALUES ('Tie Fighter', 1);
+INSERT INTO spaceship (name, show_id) VALUES ('Enterprise', 2);
+INSERT INTO spaceship (name, show_id) VALUES ('Helicarrier', 3);
